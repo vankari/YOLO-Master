0ede44887492076c25e26b7e6c9eef2c2dfbfcbf
Andy Liu <andyliu.edu@gmail.com>
2026-07-07 21:48:33 -0700

    feat: Add MNN backend support for YOLO-Master Edge Deployment

--- examples/YOLO-Master-Edge-Deployment/cpp/backends/mnn_backend.h
+++ examples/YOLO-Master-Edge-Deployment/cpp/backends/mnn_backend.h
@@ -2,13 +2,24 @@
 
 #include "backend.h"
 
+#ifdef WITH_MNN
+#include <MNN/Interpreter.hpp>
+#endif
+
 class MnnBackend final : public Backend {
 public:
+    MnnBackend();
+    ~MnnBackend() override;
     void load(const std::string& model_path) override;
     Tensor infer(const Tensor& input) override;
     std::string name() const override;
 
 private:
     std::string model_path_;
+#ifdef WITH_MNN
+    MNN::Interpreter* interpreter_ = nullptr;
+    MNN::Session* session_ = nullptr;
+    MNN::Tensor* input_tensor_ = nullptr;
+#endif
 };
 
